d9e47eca9a647f91750d046a12205ea796e9aea2
mx5566 <2473321894@qq.com>
2020-12-31 18:04:20 +0800

    1

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mx5566/log v0.0.0-20201231093454-bed824f075e6
+	github.com/mx5566/logm v0.0.0-20201231100214-d76014dd520f
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
-import "log"
-import "github.com/mx5566/pagpiao/logic"
+import (
+	"github.com/mx5566/logm"
+	"github.com/mx5566/pagpiao/logic"
+)
 
 func main() {
-
 	var err error
 	err = logic.GetStockListA(".\\sseA.csv")
 	if err != nil {
-		log.Fatal(err)
-	}
-	err = logic.GetStockListB(".\\sseB.csv")
-	if err != nil {
-		log.Fatal(err)
+		logm.FatalE(err)
 	}
 
+	err = logic.GetStockListB(".\\sseB.csv")
+	if err != nil {
+		logm.FatalE(err)
+	}
 }
